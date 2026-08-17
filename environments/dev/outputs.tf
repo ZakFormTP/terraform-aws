201cfdd32d424a1df8bf6a6780970706b47efb7f
ZakFormTP <boualiazak@gmail.com>
2026-08-17 15:12:19 +0200

    Update dev/outputs.tf

--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -13,6 +13,12 @@ output "test_account_id" {
   value = data.aws_caller_identity.current.account_id
 }
 
+# Affiche la commande de connexion complète prête à l'emploi - password
+output "password_connection_command" {
+  description = "Commande pour se connecter à l'instance EC2 avec un mot de passe"
+  value       = "ssh mon_utilisateur@${module.ec2-dev.public_ip}"
+}
+
 # Affiche la commande SSH complète prête à l'emploi
 output "ssh_connection_command" {
   description = "Commande pour se connecter à l'instance EC2"
